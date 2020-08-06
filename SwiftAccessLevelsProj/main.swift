4f0e579a793d911c9476fe3463f4dec6457f5bd8
Artem <atkachuk@uttpic.com>
2020-08-05 22:19:12 -0700

    Initial Commit

---
+++ SwiftAccessLevelsProj/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftAccessLevelsProj
+//
+//  Created by Artem Tkachuk on 8/5/20.
+//  Copyright © 2020 Artem Tkachuk. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
